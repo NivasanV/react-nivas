38230b3ca1d2749bac8ea1efd11442b610cd037e
NivasanV <dhivya2002py@gmail.com>
2022-01-23 15:15:18 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import './style.css';
+import './img.png';
 
 var element = <h2 className="firstAttribute">google</h2>;
 
 var mystyle = {
-  fontSize: 50,  
-  fontFamily: 'Courier',  
-  color: '#003300'
+  fontSize: 50,
+  fontFamily: 'Courier',
+  color: '#003300',
 };
 
-var ref="https://www.javatpoint.com/react-components";
+var img = <img src="./img.png"></img>;
+
+var ref = 'https://www.javatpoint.com/react-components';
 
 function App() {
   return (
     <div>
-      <h1 style={mystyle}><a href={ref}>{element}{2+2}</a></h1>
+      <h1 style={mystyle}>
+        <a href={ref}>
+          {element}
+          {2 + 2}
+        </a>
+        {img}
+      </h1>
     </div>
   );
 }
