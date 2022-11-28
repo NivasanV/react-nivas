53743720363e09e3af93da29fca1679682981810
NivasanV <58383171+NivasanV@users.noreply.github.com>
2022-11-28 12:03:22 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
 import React from 'react';
 import './style.css';
 
-function App() {
-  return <div>hi</div>;
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
 }
-export default App;
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <div className="avatar"></div>
+      <AboutPage/>
+      <MyButton />
+      <Profile/>
+    </div>
+  );
+}
